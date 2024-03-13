9c10aee49bfcfccf14ad56b579eb545a6b93e588
raissalotin <rahlotin@gmail.com>
2024-03-13 16:09:12 -0300

    add alert resize window

--- src/composables/screen.js
+++ src/composables/screen.js
@@ -6,6 +6,7 @@ export function useScreen() {
   const isMobile = ref(window.innerWidth < 768);
 
   const onBrowserResize = () => {
+    alert('i')
     browserWidth.value = window.innerWidth;
     deviceWidth.value = screen.width;
     isMobile.value = window.innerWidth < 768;
